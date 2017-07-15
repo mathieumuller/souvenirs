5eadd6d21b0641e237251052875f08dfabd6ac30
Unknown <mathieu.muller1006@gmail.com>
2017-07-15 02:53:32 +0200

    ready to work on ubuntu

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // webpack.config.js
-var Encore=require('@symfony/webpack-encore');
+var Encore = require('@symfony/webpack-encore');
 
 Encore
     // directory where all compiled assets will be stored
@@ -18,18 +18,15 @@ Encore
     .addStyleEntry('global', './assets/css/global.scss')
 
     // allow sass/scss files to be processed
-    .enableSassLoader({
-        resolve_url_loader: false
-    })
+    .enableSassLoader()
 
     // allow legacy applications to use $/jQuery as a global variable
     .autoProvidejQuery()
 
-    .enableSourceMaps(!Encore.isProduction())
+    .enableSourceMaps(!Encore.isProduction());
 
     // create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning()
-;
 
 // export the final configuration
 module.exports = Encore.getWebpackConfig();
